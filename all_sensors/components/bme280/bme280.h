b6af32bbd6106051fd9e20bfa68ad8c6251b1cd5
bez_kofeinowa <margaj@student.agh.edu.pl>
2026-01-05 18:09:01 +0100

    wersja beta

---
+++ all_sensors/components/bme280/bme280.h
@@ -0,0 +1,9 @@
+#ifndef BME280_H
+#define BME280_H
+
+#include "driver/i2c.h"
+
+esp_err_t bme280_init(i2c_port_t port, uint8_t addr);
+esp_err_t bme280_read_float_data(i2c_port_t port, uint8_t addr, float *temp, float *press, float *hum);
+
+#endif
